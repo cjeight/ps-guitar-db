4fe52df6cedadc9b6d51e1a9d8977c2cebf291d9
Davidson, Marc (FAA) <marc.davidson@faa.gov>
2018-11-14 18:48:45 -0600

    End of Session two

--- src/main/java/com/guitar/db/repository/ManufacturerRepository.java
+++ src/main/java/com/guitar/db/repository/ManufacturerRepository.java
@@ -1,51 +1,48 @@
 package com.guitar.db.repository;
 
-import java.util.Date;
-import java.util.List;
+import com.guitar.db.model.Manufacturer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-
-import org.springframework.stereotype.Repository;
-
-import com.guitar.db.model.Manufacturer;
+import java.util.Date;
+import java.util.List;
 
 @Repository
 public class ManufacturerRepository {
 	@PersistenceContext
 	private EntityManager entityManager;
 
+	@Autowired
+	private ManufacturerJpaRepository manufacturerJpaRepository;
+
 	/**
 	 * Create
 	 */
 	public Manufacturer create(Manufacturer man) {
-		entityManager.persist(man);
-		entityManager.flush();
-		return man;
+		return manufacturerJpaRepository.saveAndFlush(man);
 	}
 
 	/**
 	 * Update
 	 */
 	public Manufacturer update(Manufacturer man) {
-		man = entityManager.merge(man);
-		entityManager.flush();
-		return man;
+		return manufacturerJpaRepository.saveAndFlush(man);
 	}
 
 	/**
 	 * Delete
 	 */
 	public void delete(Manufacturer man) {
-		entityManager.remove(man);
-		entityManager.flush();
+		manufacturerJpaRepository.delete(man);
 	}
 
 	/**
 	 * Find
 	 */
 	public Manufacturer find(Long id) {
-		return entityManager.find(Manufacturer.class, id);
+		return manufacturerJpaRepository.findOne(id);
 	}
 
 	/**
